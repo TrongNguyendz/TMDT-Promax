ba20ce4adb10377cfe6faf1414de757c962a089e
LeeQuin04 <lequyen22122004@gmail.com>
2026-02-26 09:07:09 +0700

    26/2:Thêm folder cus và sửa đường dẫn import

--- fe/src/pages/customer/AuthPage.vue
+++ fe/src/pages/customer/AuthPage.vue
@@ -149,7 +149,7 @@ import { useRoute, useRouter } from 'vue-router';
 import { useUserStore } from '../../stores/user';
 import { useUIStore } from '../../stores/ui';
 import { Login, Register, ForgotPassword } from '../../utils/user_service_api';
-import bgImage from '../assets/auth.jpg';
+import bgImage from '../../assets/auth.jpg';
 import axios from 'axios';
 
 const route = useRoute();
