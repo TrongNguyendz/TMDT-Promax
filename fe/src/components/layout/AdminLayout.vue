28fdea0d12fc2ac2b8b2534008e9920db42c7ef0
Anhnguyen <baanh9343@gmail.com>
2026-03-09 09:22:29 +0700

    front ghtk

--- fe/src/components/layout/AdminLayout.vue
+++ fe/src/components/layout/AdminLayout.vue
@@ -45,7 +45,7 @@
             <NavItem to="/admin/vouchers" label="Vouchers" :active="$route.path.startsWith('/admin/vouchers')">
               <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"/></svg>
             </NavItem>
-            <NavItem to="/admin/travel" label="Travel" :active="$route.path.startsWith('/admin/travel')">
+            <NavItem to="/admin/transport" label="Transport" :active="$route.path.startsWith('/admin/transport')">
               <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 11m8 4V21M4 11v10l8 4s" /></svg>
             </NavItem>  
             </div>
