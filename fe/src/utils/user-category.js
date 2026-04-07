369b50a65a3cb859bbfdbff5369ddf6e0a63c1f3
anhnguyen <baanh9343@gmail.com>
2026-04-07 11:19:56 +0700

    done category

--- fe/src/utils/user-category.js
+++ fe/src/utils/user-category.js
@@ -1,32 +1,37 @@
-import axios from "axios"; 
+import axios from "axios";
 
-export const postuserCategoryInteraction = async (payload,token) => {
+export const postuserCategoryInteraction = async (payload, token) => {
   // payload là object { categoryId, userId, ... }
 
   try {
-    const response = await axios.post('http://localhost:3000/api/v1/user-category/click', payload, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`   // nếu backend dùng JWT trong header thì thêm vào đây
-      }
-    });
+    const response = await axios.post(
+      "http://localhost:3000/api/v1/user-category/click",
+      payload,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // nếu backend dùng JWT trong header thì thêm vào đây
+        },
+      },
+    );
     return response.data;
   } catch (error) {
-    console.error('Full error response:', error.response?.data || error.message);
+    console.error(
+      "Full error response:",
+      error.response?.data || error.message,
+    );
     throw error;
   }
 };
 
+// user-category.js
 export const getRecentCategoryIds = async (userId, token) => {
-    if (!token) throw new Error('Token is required');
-    return await axios.get(
-        `http://localhost:3000/api/v1/user-category/recent`,
-        { "userId": userId },
-        {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
-            }, 
-        }
-    );
-}
+  const response = await axios.get(
+    `http://localhost:3000/api/v1/user-category/recent`, 
+    {
+      params: { userId: userId }, // Nó sẽ tạo ra dạng ?userId=1000
+      headers: { Authorization: `Bearer ${token}` }
+    }
+  );
+  return response.data.data; 
+};
