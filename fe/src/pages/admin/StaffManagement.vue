4e5513618eef4851e59e6bd152a4007b153ae363
Lord-Eternal <et3rnxl24@gmail.com>
2026-04-07 11:36:52 +0700

    committing

--- fe/src/pages/admin/StaffManagement.vue
+++ fe/src/pages/admin/StaffManagement.vue
@@ -464,27 +464,27 @@ const loadStaff = async () => {
 // Shifts Data
 const shifts = ref([]);
 
-// async function loadShifts() {
-//   try {
-//     const res = await getListShifts(token.value);
-//     if (!res.data.success) return;
+async function loadShifts() {
+  try {
+    const res = await getListShifts(token.value);
+    if (!res.data.success) return;
 
-//     shifts.value = res.data.data.map((s) => {
-//       const date = new Date(s.shift_date).toISOString().split("T")[0];
-//       return {
-//         id: s.id,
-//         staffId: s.staff_id,
-//         date: date,
-//         start: `${date}T${s.start_time}`,
-//         end: `${date}T${s.end_time}`,
-//         notes: s.notes,
-//         color: s.color || '#3b82f6'
-//       };
-//     });
-//   } catch (err) {
-//     console.error("Load shifts failed:", err);
-//   }
-// }
+    shifts.value = res.data.data.map((s) => {
+      const date = new Date(s.shift_date).toISOString().split("T")[0];
+      return {
+        id: s.id,
+        staffId: s.staff_id,
+        date: date,
+        start: `${date}T${s.start_time}`,
+        end: `${date}T${s.end_time}`,
+        notes: s.notes,
+        color: s.color || '#3b82f6'
+      };
+    });
+  } catch (err) {
+    console.error("Load shifts failed:", err);
+  }
+}
 
 // Filters
 const filters = reactive({
