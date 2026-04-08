c88418e3a3167038d1640f746811f04a8f4f596a
anhnguyen <baanh9343@gmail.com>
2026-04-08 11:44:39 +0700

    profile

--- fe/src/pages/customer/ProfilePage.vue
+++ fe/src/pages/customer/ProfilePage.vue
@@ -148,7 +148,7 @@ const isUpdatingAvatar = ref(false) ;
 // const avatarPreview = ref(user.profile?.avatar_url || "");
 const avatarPreview = computed(() => {
   if (!user.profile?.avatar_url) return defaultAvatar;
-  return `http://localhost:3001${user.profile.avatar_url}`;
+  return `https://tmdt-promax-user-service.onrender.com${user.profile.avatar_url}`;
 });
 const avatarFile = ref(null); // File để gửi lên server
 
