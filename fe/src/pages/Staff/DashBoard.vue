39bb37803bdee6b4177ba689432e2fde86a24c31
anhnguyen <baanh9343@gmail.com>
2026-04-08 10:33:21 +0700

    fixfe

--- fe/src/pages/Staff/DashBoard.vue
+++ fe/src/pages/Staff/DashBoard.vue
@@ -106,7 +106,7 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue';
 import orderApi from '@/utils/order_service_api';
-import { getTopProducts } from '../../utils/product_service_api';
+import { getlistproduct } from '../../utils/product_service_api';
 import ProductCard from '../../components/common/ProductCard.vue';
 
 const today = computed(() => new Date().toLocaleDateString('vi-VN', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }));
@@ -174,7 +174,7 @@ const topProducts = ref([]);
 
 const loadTopProducts = async () => {
   try {
-    const res = await getTopProducts()
+    const res = await getlistproduct()
 
     const data = res.data.data || []
 
